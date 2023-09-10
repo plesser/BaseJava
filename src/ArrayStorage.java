bbd1cb25eae4586b000ab85621bb0ded2799b48a
Oleg Plesser <oleg@plesser.ru>
2023-09-10 19:47:18 +0300

    ArrayStorage Методы сделай public (кроме getIndex) update, save, delete Удали пустые строки save Сделай условия по принципу if - else if - else getAll Закомментированный код удали

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,33 +8,25 @@ public class ArrayStorage {
     Resume[] storage = new Resume[CAPACITY];
     int countResumes = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, null);
         countResumes = 0;
     }
 
-    void update(Resume r) {
-
+    public void update(Resume r) {
         int index = getIndex(r.uuid);
         if (index != -1) {
             storage[index] = r;
         } else {
             System.out.println("ERROR UPDATE: Don't find resume with uuid " + r.uuid);
         }
-
     }
 
-
-    void save(Resume r) {
-
+    public void save(Resume r) {
         int index = getIndex(r.uuid);
-
         if (countResumes == storage.length){
             System.out.println("ERROR: storage is full");
-            return;
-        }
-
-        if (index == -1 && countResumes <= storage.length) {
+        } else if (index == -1 && countResumes <= storage.length) {
             storage[countResumes] = r;
             countResumes++;
         } else {
@@ -42,7 +34,7 @@ public class ArrayStorage {
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index != -1){
             return storage[index];
@@ -52,8 +44,7 @@ public class ArrayStorage {
         }
     }
 
-    void delete(String uuid) {
-
+    public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index != -1) {
             storage[index] = storage[countResumes - 1];
@@ -67,13 +58,11 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        //Resume[] allResume = new Resume[countResumes];
-        //System.arraycopy(storage, 0, allResume, 0, countResumes);
+    public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, countResumes);
     }
 
-    int size() {
+    public int size() {
         return countResumes;
     }
 
